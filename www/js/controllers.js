908c145d8a2eb051052594e28c0b44deb41805cb
Tejas <tejas@kinvey.com>
2016-05-05 11:49:26 -0400

    logout fix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -375,6 +375,7 @@ angular.module('app.controllers', ['chart.js', 'ngCordova'])
         },
         function (error) {
           //Kinvey logout finished with error
+          $state.go('login');
           alert("Error logout: " + JSON.stringify(error));
     });
 }])
